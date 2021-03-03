5c446dc47916ada4b7ffdabf0ed543eae69841fc
wwalpha <wwalpha@gmail.com>
2021-03-03 11:38:42 +0000

    add output

--- dist/src/client.js
+++ dist/src/client.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.client = exports.documentClient = void 0;
 const aws_sdk_1 = require("aws-sdk");
 const aws_xray_sdk_core_1 = __importDefault(require("aws-xray-sdk-core"));
-aws_xray_sdk_core_1.default.config([aws_xray_sdk_core_1.default.plugins.EC2Plugin]);
+aws_xray_sdk_core_1.default.enableAutomaticMode();
 aws_xray_sdk_core_1.default.setContextMissingStrategy('LOG_ERROR');
 /**
  * table data item client
@@ -14,16 +14,18 @@ aws_xray_sdk_core_1.default.setContextMissingStrategy('LOG_ERROR');
 const documentClient = (options = {
     region: process.env.AWS_DEFAULT_REGION,
 }) => {
-    console.log('xray', options.xray);
-    // if (options.xray === true) {
-    //   AWSXRay.enableAutomaticMode();
-    // } else {
-    //   AWSXRay.enableManualMode();
-    // }
-    const AWS = aws_xray_sdk_core_1.default.captureAWS(require('aws-sdk'));
-    aws_xray_sdk_core_1.default.setContextMissingStrategy('LOG_ERROR');
-    const client = new AWS.DynamoDB.DocumentClient(options);
-    return client;
+    if (!options.region) {
+        options.region = process.env.AWS_DEFAULT_REGION;
+    }
+    if (options.xray === true) {
+        const client = new aws_sdk_1.DynamoDB.DocumentClient({
+            service: new aws_sdk_1.DynamoDB(options),
+            ...options,
+        });
+        aws_xray_sdk_core_1.default.captureAWSClient(client.service);
+        return client;
+    }
+    return new aws_sdk_1.DynamoDB.DocumentClient(options);
 };
 exports.documentClient = documentClient;
 /**
@@ -34,12 +36,13 @@ exports.documentClient = documentClient;
 const client = (options = {
     region: process.env.AWS_DEFAULT_REGION,
 }) => {
+    if (!options.region) {
+        options.region = process.env.AWS_DEFAULT_REGION;
+    }
+    const client = new aws_sdk_1.DynamoDB(options);
     if (options.xray === true) {
-        aws_xray_sdk_core_1.default.enableAutomaticMode();
+        return aws_xray_sdk_core_1.default.captureAWSClient(client);
     }
-    else {
-        aws_xray_sdk_core_1.default.enableManualMode();
-    }
-    return new aws_sdk_1.DynamoDB(options);
+    return client;
 };
 exports.client = client;
